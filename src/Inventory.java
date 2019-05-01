67a99dc29d981fd00337b1944c7cdb5a144f1357
Tempest123 <ujjanthcol@gmail.com>
2019-05-01 22:17:20 +0530

    Table added

--- src/Inventory.java
+++ src/Inventory.java
@@ -8,6 +8,9 @@ import javax.swing.JScrollPane;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
+import javax.swing.ScrollPaneConstants;
 
 public class Inventory extends JFrame {
 
@@ -29,42 +32,45 @@ public class Inventory extends JFrame {
 				}
 			}
 		});
-		showeverything();
 	}
 	
-	
-
-	private static void showeverything() {
-		obj.show();//needed!
-		System.out.println("here!");
-		records = new String[obj.records.length];
-		for(int i = 0;i < obj.records.length;i++) {
-			records[i] = obj.records[i];
-			System.out.println(records[i]);
-		}
-		
-	}
 
 	/**
 	 * Create the frame.
 	 */
 	public Inventory() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 372, 491);
+		setBounds(100, 100, 434, 491);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 0, 2, 2);
+		obj.show();//needed!
+		records = new String[obj.records.length];
+		for(int i = 0;i < obj.records.length;i++) {
+			records[i] = obj.records[i];
+			System.out.println(records[i]);
+		}
+		
+		int numberoffields = 5;
+		String words[] = new String[numberoffields];
+		String[] columnNames = {"ID","Medicine Name","Stock","Date","Company"};
+		Object[][] data = new String[records.length][numberoffields];
+		for(int k = 0; k < records.length;k++) {
+			words = records[k].split("\\|");
+			for(int j = 0; j < numberoffields; j++) {
+				data[k][j] = new String(words[j]);
+				System.out.println(data[k][j]+"\t");
+			}
+		}
+		JTable myTable = new JTable(data,columnNames);
+		myTable.setFillsViewportHeight(true);
+		JScrollPane scrollPane = new JScrollPane(myTable);
+		scrollPane.setBounds(10, 11, 398, 430);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		contentPane.add(scrollPane);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel.setBounds(81, 68, 185, 43);
-		contentPane.add(lblNewLabel);
 	}
-
 }
